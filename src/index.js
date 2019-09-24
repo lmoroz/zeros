bfde7404c0cf04f964b5105d5499b9b61f5a58bf
Larisa Morozhnikova <larisa.moroz@gmail.com>
2019-09-25 05:28:34 +0800

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 module.exports = function zeros(expression) {
-  // your solution
-}
+    expression = expression.split('*').map(value => {
+        let arr = [];
+        let m = value.indexOf('!!') === -1 ? 1 : 2;
+        value = parseInt(value, 10);
+
+        for (; value > 0; value -= m) arr.push(value);
+        return arr;
+    });
+
+    let twos = 0;
+    let fives = 0;
+
+    expression.forEach(value => {
+        value.forEach(value => {
+            for (let num = value; num % 2 === 0; num /= 2) twos++;
+            for (let num = value; num % 5 === 0; num /= 5) fives++;
+        });
+    });
+
+    return fives > twos ? twos : fives;
+};
